5bdf6e55850827603923dd2ca023562248dfc479
SweetSnowyWitch <snowysatan@yandex.ru>
2023-01-25 16:26:32 +0500

    Start

---
+++ src/main/java/csmp/CsmpClient.java
@@ -0,0 +1,13 @@
+package csmp;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class CsmpClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/csmp/CsmpMain.java
@@ -0,0 +1,10 @@
+package csmp;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CsmpMain implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
