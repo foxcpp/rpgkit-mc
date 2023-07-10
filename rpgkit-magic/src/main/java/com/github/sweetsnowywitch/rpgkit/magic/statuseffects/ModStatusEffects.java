8293d4a73c6addb8e9aee81fb23ab9d4efa369dc
fox.cpp <fox.cpp@disroot.org>
2023-07-10 13:51:38 +0300

    magic: Add KnockbackResistanceEffect as a status effect

--- rpgkit-magic/src/main/java/com/github/sweetsnowywitch/rpgkit/magic/statuseffects/ModStatusEffects.java
+++ rpgkit-magic/src/main/java/com/github/sweetsnowywitch/rpgkit/magic/statuseffects/ModStatusEffects.java
@@ -1,6 +1,8 @@
 package com.github.sweetsnowywitch.rpgkit.magic.statuseffects;
 
 import com.github.sweetsnowywitch.rpgkit.magic.RPGKitMagicMod;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -8,9 +10,14 @@ import net.minecraft.util.registry.Registry;
 public class ModStatusEffects {
     public static final StatusEffect MUTE = new MuteStatusEffect();
     public static final StatusEffect SEALED = new SealedStatusEffect();
+    public static final StatusEffect KNOCKBACK_RESISTANCE = (new KnockbackResistanceEffect()).addAttributeModifier(
+            EntityAttributes.GENERIC_KNOCKBACK_RESISTANCE, "0fff0d1e-1f0c-11ee-9b61-00155d11d75f",
+            0.3, EntityAttributeModifier.Operation.ADDITION
+    );
 
     public static void register() {
         Registry.register(Registry.STATUS_EFFECT, Identifier.of(RPGKitMagicMod.MOD_ID, "sound_mute"), MUTE);
         Registry.register(Registry.STATUS_EFFECT, Identifier.of(RPGKitMagicMod.MOD_ID, "sealed"), SEALED);
+        Registry.register(Registry.STATUS_EFFECT, Identifier.of(RPGKitMagicMod.MOD_ID, "knockback_resistance"), KNOCKBACK_RESISTANCE);
     }
 }
