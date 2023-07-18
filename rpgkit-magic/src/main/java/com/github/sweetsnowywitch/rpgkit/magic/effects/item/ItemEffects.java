5fd99427559fa4fa573836c200d04cf43df869c0
fox.cpp <fox.cpp@disroot.org>
2023-07-18 19:36:57 +0300

    magic: Add CookItemEffect, ConsumeItemEffect, CookItemEffect item effects

--- rpgkit-magic/src/main/java/com/github/sweetsnowywitch/rpgkit/magic/effects/item/ItemEffects.java
+++ rpgkit-magic/src/main/java/com/github/sweetsnowywitch/rpgkit/magic/effects/item/ItemEffects.java
@@ -9,5 +9,8 @@ public class ItemEffects {
     public static void register() {
         Registry.register(MagicRegistries.ITEM_EFFECTS, new Identifier(RPGKitMagicMod.MOD_ID, "transmute_item"), TransmuteItemEffect::new);
         Registry.register(MagicRegistries.ITEM_EFFECTS, new Identifier(RPGKitMagicMod.MOD_ID, "give_item"), GiveItemEffect::new);
+        Registry.register(MagicRegistries.ITEM_EFFECTS, new Identifier(RPGKitMagicMod.MOD_ID, "cook_item"), CookItemEffect::new);
+        Registry.register(MagicRegistries.ITEM_EFFECTS, new Identifier(RPGKitMagicMod.MOD_ID, "caster_apply"), CasterApplyEffect::new);
+        Registry.register(MagicRegistries.ITEM_EFFECTS, new Identifier(RPGKitMagicMod.MOD_ID, "consume_item"), ConsumeItemEffect::new);
     }
 }
